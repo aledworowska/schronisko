23c7e031df515eedaf81c113124995159530668a
unknown <michalowskimilosz@gmail.com>
2022-03-22 23:14:28 +0100

    add Spring Initializr project with dependencies

---
+++ Bachelor/src/test/java/ug/bachelor/BachelorApplicationTests.java
@@ -0,0 +1,13 @@
+package ug.bachelor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BachelorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
