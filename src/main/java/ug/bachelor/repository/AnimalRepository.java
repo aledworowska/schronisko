c10fd623d853852fbb11e5701884c7437e5c487e
unknown <michalowskimilosz@gmail.com>
2022-04-19 18:59:49 +0200

    add postgresql database settings

--- src/main/java/ug/bachelor/repository/AnimalRepository.java
+++ src/main/java/ug/bachelor/repository/AnimalRepository.java
@@ -4,6 +4,9 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import ug.bachelor.domain.Animal;
 
+import javax.transaction.Transactional;
+
 @Repository
+@Transactional
 public interface AnimalRepository extends CrudRepository<Animal, Long> {
 }
